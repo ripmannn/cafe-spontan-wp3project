868776f924b2882e5c8abe5b028ab286136c374f
ariefrahmanpratama <rahmanarief111@gmail.com>
2025-01-24 15:23:18 +0700

    Menghilangkan kode sensitif

--- application/config/midtrans.php
+++ application/config/midtrans.php
@@ -7,7 +7,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // production
 
-// $config['MIDTRANS_SERVER_KEY'] = 'Mid-server-IOArA0rcvbwHDL7EZhW9NKxu'; // Ganti dengan server key Anda
-// $config['MIDTRANS_CLIENT_KEY'] = 'Mid-client-fF-G1hcvlXSJXcp6'; // Ganti dengan client key Anda
-// $config['MIDTRANS_IS_SANDBOX'] = false; // Tentukan true jika menggunakan sandbox
+// tambahkan kode production dari midtrans
 ?>
